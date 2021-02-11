f20a1e3a20b3a554e6fbaf3dce7a3e9658f328b7
kalesha <kalesha.pasha@gmail.com>
2021-02-11 19:30:14 +0530

    third commit

---
+++ test_string.py
@@ -0,0 +1,14 @@
+txt="hello this is kalesha"
+##length of the string
+print(len(txt))
+
+##looping the string and printing the every word
+for x in txt:
+ print(x)
+
+##checking if the word is present in given string
+if "this" in txt:
+ print("this is in given string")
+ 
+
+
