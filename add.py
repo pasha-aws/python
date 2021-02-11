dbbf887671300aefaf1cb672a91c1426dfdc5927
kalesha <kalesha.pasha@gmail.com>
2021-02-11 19:20:40 +0530

    second commit

---
+++ add.py
@@ -0,0 +1,22 @@
+x=5
+y=10
+
+def add():
+ z=x+y
+ print(z)
+
+add()
+
+def substract():
+ u=y-x
+ print(u)
+substract()
+
+st="hello this is kalesha"
+def find():
+ if "this" in st:
+  print("this is exit in"+st)
+find() 
+
+txt="kalesha"
+for
